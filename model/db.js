9fc1cac3ad0d6a7e2696b369d4b4a3ab69eb2550
shilovskiy <shilovskiy@gmail.com>
2016-12-26 21:16:43 +0300

    db commented

--- model/db.js
+++ model/db.js
@@ -9,30 +9,30 @@ exports.mongoose = mongoose;
 
 var MongoClient = require('mongodb').MongoClient;
 
-var state = {
-    db: null,
-};
-
-exports.connect = function(dburl, done) {
-    if (state.db) return done;
-
-    MongoClient.connect(dburl, function(err, db) {
-        if (err) return done(err);
-        state.db = db;
-
-    })
-};
-
-exports.get = function() {
-    return state.db
-};
-
-exports.close = function(done) {
-    if (state.db) {
-        state.db.close(function(err, result) {
-            state.db = null;
-            state.mode = null;
-            //done(err)
-        })
-    }
-};
+// var state = {
+//     db: null,
+// };
+//
+// exports.connect = function(dburl, done) {
+//     if (state.db) return done;
+//
+//     MongoClient.connect(dburl, function(err, db) {
+//         if (err) return done(err);
+//         state.db = db;
+//
+//     })
+// };
+//
+// exports.get = function() {
+//     return state.db
+// };
+//
+// exports.close = function(done) {
+//     if (state.db) {
+//         state.db.close(function(err, result) {
+//             state.db = null;
+//             state.mode = null;
+//             //done(err)
+//         })
+//     }
+// };

f18652785a174c7bb27f6f2cd8ce032ec33f15f9
shilovskiy <shilovskiy@gmail.com>
2017-03-12 18:35:09 +0300

    DB Schema

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ var search = require('./routes/search');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 
 app.use(logger('dev'));

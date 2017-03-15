f7f811917f5b1c23fe008ed166524b9e09eb11bb
shilovskiy <shilovskiy@gmail.com>
2017-03-15 22:19:10 +0300

    add headers

--- app.js
+++ app.js
@@ -30,6 +30,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Project Routes
 app.use('/', index);
 app.use('/login', user);
